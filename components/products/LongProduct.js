d98d870889612424e4769ea9bece49fcd22329dd
= <=>
2021-09-08 00:21:39 +0600

    longProduct bottomIcons

--- components/products/LongProduct.js
+++ components/products/LongProduct.js
@@ -1,21 +1,27 @@
 import styles from "../../styles/product/longproduct.module.scss";
 import { DefaultRating } from "../common/Ratings";
-import { FavoriteBorder, Visibility, ShoppingCart } from "@material-ui/icons";
+import { Favorite, Visibility, ShoppingCart } from "@material-ui/icons";
 import { longProduct as data } from "../../utils/data";
 import { SingleIcon } from "../common/SingleComponent";
 import { NormalButton } from "../common/Button";
 
 export default function LongProductCard() {
+  const sideIcon = true;
   return (
     <div className={styles.slideProduct}>
-      <RenderIcon />
+      <span className={styles.discount}>25%</span>
+      {sideIcon && <RenderIcon />}
 
       <div className={styles.image}>
         <img src={data.imgUrl} alt="products-1" />
+        {!sideIcon && <RenderBottomIcon />}
       </div>
 
       <div className={styles.content}>
-        <DefaultRating value={parseInt(data.reviewCount)} totalViews={data.totalViews} />
+        <DefaultRating
+          value={parseInt(data.reviewCount)}
+          totalViews={data.totalViews}
+        />
 
         <h3>{data.productName}</h3>
 
@@ -23,7 +29,7 @@ export default function LongProductCard() {
           ${data.price} <span>${data.oldPrice}</span>
         </div>
 
-        <NormalButton text="add to cart"/>
+        <NormalButton text="add to cart" />
       </div>
     </div>
   );
@@ -32,14 +38,32 @@ export default function LongProductCard() {
 const RenderIcon = () => {
   return (
     <div className={styles.icons}>
-      <SingleIcon url={"/wishlists"}>
-        <FavoriteBorder className={styles.iconsize} />
+      <SingleIcon url={"/wishlists"} className={styles.anc}>
+        <Favorite className={styles.iconsize} />
       </SingleIcon>
-      <SingleIcon url={"/cartlists"}>
+      <SingleIcon url={"/cartlists"} className={styles.anc}>
         <ShoppingCart className={styles.iconsize} />
       </SingleIcon>
-      <SingleIcon url={"/detail"}>
+      <SingleIcon url={"/detail"} className={styles.anc}>
         <Visibility className={styles.iconsize} />
+      </SingleIcon>
+    </div>
+  );
+};
+const RenderBottomIcon = () => {
+  return (
+    <div className={styles.bottomIcons}>
+      <SingleIcon url={"/wishlists"} className={styles.iconsize}>
+        <Favorite style={{ fontSize: "2.5rem" }} />
+      </SingleIcon>
+      <SingleIcon
+        url={"/cartlists"}
+        className={`${styles.cartBtn} ${styles.iconsize}`}
+      >
+        Add to cart
+      </SingleIcon>
+      <SingleIcon url={"/detail"} className={styles.iconsize}>
+        <Visibility style={{ fontSize: "2.5rem" }} />
       </SingleIcon>
     </div>
   );
